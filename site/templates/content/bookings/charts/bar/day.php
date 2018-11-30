b9ec43724af60c754a2f45433d88ce55ce04e5e3
bbullemer <barbara.bullemer@gmail.com>
2018-11-30 13:10:27 -0600

    Morris Charts for Bookings
    - created pie chart for daily bookings
    - added lines for each warehouse for yearly bookings line chart

--- site/templates/content/bookings/charts/bar/day.php
+++ site/templates/content/bookings/charts/bar/day.php
@@ -1,52 +1,71 @@
-<?php use Dplus\Base\DplusDateTime; ?>
+<?php
+	use Dplus\Base\DplusDateTime;
+
+	$salesgroups = get_bookingsalesgroups();
+	$colors = array_rand(array_flip($config->allowedcolors), sizeof($salesgroups));
+
+	$bardata['data'] = array(
+		'today' => DplusDateTime::format_date($date, 'F dS Y')
+	);
+	$piedata = array();
+
+	foreach ($salesgroups as $salesgroup) {
+		$bardata['labels'][$salesgroup] = $config->booking_groups[$salesgroup];
+		$bardata['data'][$salesgroup] = get_bookingsalesgroup_day($salesgroup, $date);
+
+		$piedata[] = array(
+			'label' => $config->booking_groups[$salesgroup],
+			'value' => $bardata['data'][$salesgroup]
+		);
+	}
+?>
 <div>
-    <h3 class="mt-4">Today</h3>
-    <div id="sales-group-today"></div>
-    
-    <script>
+	<h3 class="mt-4">Today</h3>
+	<div id="sales-group-today-bar" class="form-group"></div>
+
+	<div id="sales-group-today-pie" class="form-group"></div>
+	<script>
 		$(function() {
-			if ($('#sales-group-today').hasParent('.tab-pane')) {
-				var tab = $('#sales-group-today').closest('.tab-pane');
-				
+			if ($('#sales-group-today-bar').hasParent('.tab-pane')) {
+				var tab = $('#sales-group-today-bar').closest('.tab-pane');
+
 				if (tab.hasClass('active')) {
 					draw_daybargraph();
+					draw_daypiegraph();
 				}
-				
+
 				$('a[data-toggle="tab"][href="#'+tab.attr('id')+'"]').on('shown.bs.tab', function (e) {
 					draw_daybargraph();
+					draw_daypiegraph();
 				})
-				
+
 				$('a[data-toggle="tab"][href!="#'+tab.attr('id')+'"]').on('hidden.bs.tab', function(e) {
-					$('#sales-group-today').empty();
+					$('#sales-group-today-bar').empty();
+					$('#sales-group-today-pie').empty();
 				});
-				
 			} else {
 				draw_daybargraph();
-			}	
+				draw_daypiegraph();
+			}
 		});
-		
-        function draw_daybargraph() {
-			<?php 
-                $salesgroups = get_bookingsalesgroups();
-                $groups = array();
-                $data = array(
-                    'today' => DplusDateTime::format_date($date, 'F dS Y')
-                );
-                
-                foreach ($salesgroups as $salesgroup) {
-                    $groups[] = $config->booking_groups[$salesgroup];
-                    $data[$salesgroup] = get_bookingsalesgroup_day($salesgroup, $date);
-                }
-            ?>
-            Morris.Bar({
-                element: 'sales-group-today',
-                data: [
-                    <?= json_encode($data); ?>
-                ],
-                xkey: 'today',
-                ykeys: <?= json_encode($salesgroups); ?>,
-                labels: <?= json_encode($groups); ?>
-            });
+
+		function draw_daybargraph() {
+			Morris.Bar({
+				element: 'sales-group-today-bar',
+				data: <?= json_encode(array($bardata['data'])); ?>,
+				xkey: 'today',
+				ykeys: <?= json_encode($salesgroups); ?>,
+				labels: <?= json_encode(array_values($bardata['labels'])); ?>,
+				barColors: <?= json_encode($colors); ?>
+			});
 		}
-    </script>
+
+		function draw_daypiegraph() {
+			Morris.Donut({
+				element: 'sales-group-today-pie',
+				data: <?= json_encode($piedata); ?>,
+				colors: <?= json_encode($colors); ?>
+			});
+		}
+	</script>
 </div>
--- site/templates/content/bookings/charts/line/year-trend.php
+++ site/templates/content/bookings/charts/line/year-trend.php
@@ -1,58 +1,76 @@
-<?php use Dplus\Base\DplusDateTime; ?>
+<?php
+	use Dplus\Base\DplusDateTime;
+
+	$linedata = array(
+		'ykeys' => array('total'),
+		'labels' => array('Total Booked'),
+		'data' => array()
+	);
+	$salesgroups = get_bookingsalesgroups();
+
+	foreach ($salesgroups as $salesgroup) {
+		$linedata['ykeys'][] = $salesgroup;
+		$linedata['labels'][] = $config->booking_groups[$salesgroup];
+	}
+
+	$colors = array_rand(array_flip($config->allowedcolors), sizeof($linedata['ykeys']));
+
+	for ($i = 1; $i < $month; $i++) {
+		$mm = $i < 10 ? "0$i" : $i;
+		$day = "01";
+		$yyyymmdd = $year.$mm.$day;
+
+		$monthdata = array(
+			'month' => DplusDateTime::format_date($yyyymmdd, 'Y-m-d'),
+			'total' => floatval(get_bookingtotal_month($yyyymmdd))
+		);
+
+		foreach ($salesgroups as $salesgroup) {
+			$monthdata[$salesgroup] = floatval(get_bookingsalesgroup_month($salesgroup, $yyyymmdd));
+		}
+
+		$linedata['data'][] = $monthdata;
+	}
+?>
 <div>
 	<h3 class="mt-4">This Year</h3>
 	<div id="year-trend"></div>
-	
+
 	<script>
 		$(function() {
-			if ($('#year-trend').hasParent('.tab-pane')) {	
+			if ($('#year-trend').hasParent('.tab-pane')) {
 				var tab = $('#year-trend').closest('.tab-pane');
-				
+
 				if (tab.hasClass('active')) {
 					draw_yeargraph();
 				}
-				
+
 				$('a[data-toggle="tab"][href="#'+tab.attr('id')+'"]').on('shown.bs.tab', function (e) {
 					draw_yeargraph();
 				})
-				
+
 				$('a[data-toggle="tab"][href!="#'+tab.attr('id')+'"]').on('hidden.bs.tab', function(e) {
 					$('#year-trend').empty();
 				});
 			} else {
 				draw_yeargraph();
-			}	
+			}
 		});
-		
-		
-		
+
 		function draw_yeargraph() {
-			<?php 
-				$data = array();
-				
-				for ($i = 1; $i < $month; $i++) {
-					$mm = $i < 10 ? "0$i" : $i;
-					$day = "01";
-					$yyyymmdd = $year.$mm.$day;
-					$monthdata = array(
-						'month' => DplusDateTime::format_date($yyyymmdd, 'Y-m-d'),
-						'total' => get_bookingtotal_month($yyyymmdd),
-						'line2' => 100000 * $i
-					);
-					$data[] = $monthdata;
-				}
-			?>
-			Morris.Line({
+			Morris.Area({
 				element: 'year-trend',
-				data: <?= json_encode($data); ?>,
+				data: <?= json_encode($linedata['data']); ?>,
 				xkey: 'month',
 				dateFormat: function (d) {
 					var ds = new Date(d);
 					return moment(ds).format('MMMM YYYY');
 				},
-				ykeys: ['total', 'line2'],
-				labels: ['Total Booked', 'Line 2'],
+				ykeys: <?= json_encode($linedata['ykeys']); ?>,
+				labels: <?= json_encode($linedata['labels']); ?>,
+				lineColors: <?= json_encode($colors); ?>,
 				xLabelFormat: function (x) { return  moment(x).format('MMM YYYY'); },
+				behaveLikeLine: true,
 			});
 		}
 	</script>
