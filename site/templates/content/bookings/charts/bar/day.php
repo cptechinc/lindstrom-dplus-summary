875ea889cfa2cffc4f18fcc3d88ab2585ac16dfb
Paul Gomez <pauldro952@outlook.com>
2018-11-30 09:10:03 -0600

    Updated Scripts for charts to check if charts exist in tabs

--- site/templates/content/bookings/charts/bar/day.php
+++ site/templates/content/bookings/charts/bar/day.php
@@ -1,14 +1,32 @@
-<?php
-	use Dplus\Base\DplusDateTime;
-    
-?>
+<?php use Dplus\Base\DplusDateTime; ?>
 <div>
-    <h3>Today</h3>
+    <h3 class="mt-4">Today</h3>
     <div id="sales-group-today"></div>
     
     <script>
-        $(function() {
-            <?php 
+		$(function() {
+			if ($('#sales-group-today').hasParent('.tab-pane')) {	
+				var tab = $('#sales-group-today').closest('.tab-pane');
+				
+				if (tab.hasClass('active')) {
+					draw_daybargraph();
+				}
+				
+				$('a[data-toggle="tab"][href="#'+tab.attr('id')+'"]').on('shown.bs.tab', function (e) {
+					draw_daybargraph();
+				})
+				
+				$('a[data-toggle="tab"][href!="#'+tab.attr('id')+'"]').on('hidden.bs.tab', function(e) {
+					$('#sales-group-today').empty();
+				});
+				
+			} else {
+				draw_daybargraph();
+			}	
+		});
+		
+        function draw_daybargraph() {
+			<?php 
                 $salesgroups = get_bookingsalesgroups();
                 $groups = array();
                 $data = array(
@@ -32,7 +50,7 @@
                 xkey: 'today',
                 ykeys: <?= json_encode($salesgroups); ?>,
                 labels: <?= json_encode($groups); ?>
-                });
             });
+		}
     </script>
 </div>
--- site/templates/content/bookings/charts/line/year-trend.php
+++ site/templates/content/bookings/charts/line/year-trend.php
@@ -1,37 +1,57 @@
-<?php
-	use Dplus\Base\DplusDateTime;
-?>
+<?php use Dplus\Base\DplusDateTime; ?>
 <div>
-    <h3>This Year</h3>
-    <div id="year-trend"></div>
-    
-    <script>
-        $(function() {
-            <?php 
-                $data = array();
-                for ($i = 1; $i < $month; $i++) {
-                    $mm = $i < 10 ? "0$i" : $i;
-                    $day = "01";
-                    $yyyymmdd = $year.$mm.$day;
-                    $monthdata = array(
-                        'month' => DplusDateTime::format_date($yyyymmdd, 'Y-m-d'),
-                        'sales' => floatval(get_bookingtotal_month($yyyymmdd))
-                    );
-                    $data[] = $monthdata;
-                }
-            ?>
-            Morris.Line({
-                element: 'year-trend',
-                data: <?= json_encode($data); ?>,
-                xkey: 'month',
-                dateFormat: function (d) {
+	<h3 class="mt-4">This Year</h3>
+	<div id="year-trend"></div>
+	
+	<script>
+		$(function() {
+			if ($('#year-trend').hasParent('.tab-pane')) {	
+				var tab = $('#year-trend').closest('.tab-pane');
+				
+				if (tab.hasClass('active')) {
+					draw_yeargraph();
+				}
+				
+				$('a[data-toggle="tab"][href="#'+tab.attr('id')+'"]').on('shown.bs.tab', function (e) {
+					draw_yeargraph();
+				})
+				
+				$('a[data-toggle="tab"][href!="#'+tab.attr('id')+'"]').on('hidden.bs.tab', function(e) {
+					$('#year-trend').empty();
+				});
+			} else {
+				draw_yeargraph();
+			}	
+		});
+		
+		
+		
+		function draw_yeargraph() {
+			<?php 
+				$data = array();
+				for ($i = 1; $i < $month; $i++) {
+					$mm = $i < 10 ? "0$i" : $i;
+					$day = "01";
+					$yyyymmdd = $year.$mm.$day;
+					$monthdata = array(
+						'month' => DplusDateTime::format_date($yyyymmdd, 'Y-m-d'),
+						'sales' => floatval(get_bookingtotal_month($yyyymmdd))
+					);
+					$data[] = $monthdata;
+				}
+			?>
+			Morris.Line({
+				element: 'year-trend',
+				data: <?= json_encode($data); ?>,
+				xkey: 'month',
+				dateFormat: function (d) {
 					var ds = new Date(d);
 					return moment(ds).format('MMMM YYYY');
 				},
-                ykeys: ['sales'],
-                labels: ['Amount Booked'],
-                xLabelFormat: function (x) { return  moment(x).format('MMM YYYY'); },
-            });
-        });
-    </script>
+				ykeys: ['sales'],
+				labels: ['Amount Booked'],
+				xLabelFormat: function (x) { return  moment(x).format('MMM YYYY'); },
+			});
+		}
+	</script>
 </div>
